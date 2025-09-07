365e6cdcd9a1a69c2392ef6d3d573f544a94cd4e
Leonardo Cruz <leomcruzz@gmail.com>
2025-09-07 20:27:45 -0300

    testing again

--- src/main/java/com/faturium/service/DashboardService.java
+++ src/main/java/com/faturium/service/DashboardService.java
@@ -167,6 +167,7 @@ public class DashboardService {
                 .build();
     }
 
+    @Transactional
     public VendasHojeDTO previstoHoje() {
         LocalDateTime inicioHoje = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
         LocalDateTime fimHoje = inicioHoje.plusDays(1);
